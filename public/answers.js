215f2a07b9316c0f7d94abfe55cb0c89091cbee3
jboe26 <joshrboepple@outlook.com>
2020-01-02 21:25:34 -0500

    approve thisone

--- public/answers.js
+++ public/answers.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
     console.log("ready!");
 
+    $("#done").hide();
 
     $("#next1").on("click", function(event) {
         event.preventDefault();
         $(".question1").hide();
+        
         $(".question2").show();
         
     })
@@ -13,75 +15,78 @@ $(document).ready(function () {
         event.preventDefault();
         $(".question2").hide();
         $(".question3").show();
-
+        
     })
 
     $("#next3").on("click", function(event) {
         event.preventDefault();
         $(".question3").hide();
         $(".question4").show();
-
+        
     })
 
     $("#next4").on("click", function(event) {
         event.preventDefault();
         $(".question4").hide();
         $(".question5").show();
-
+        
     })
 
     $("#next5").on("click", function(event) {
         event.preventDefault();
         $(".question5").hide();
         $(".question6").show();
-
+        
     })
 
     $("#next6").on("click", function(event) {
         event.preventDefault();
         $(".question6").hide();
         $(".question7").show();
-
+        
     })
 
     $("#next7").on("click", function(event) {
         event.preventDefault();
         $(".question7").hide();
         $(".question8").show();
-
+        
     })
 
     $("#next8").on("click", function(event) {
         event.preventDefault();
         $(".question8").hide();
         $(".question9").show();
-
+        
     })
 
     $("#next9").on("click", function(event) {
         event.preventDefault();
         $(".question9").hide();
         $(".question10").show();
-
+        
     })
 
     $("#next10").on("click", function(event) {
         event.preventDefault();
         $(".question10").hide();
         $("#done").show();
+        $("#correctAnswers").show();
+        $("#missed").show();
+        $("#incorrectAnswers").show();
 
     })
     
     var correctAnswers = 0;
-    var missed = 5;
+    var missed = 10;
     var incorrectAnswers = 0;
-    var intervalID = null;
+    
    
 
 
 
     $("#done").click(function () {
-        $(".questions").hide();
+        // $(".questions").hide();
         
         evaluateQuestion("answer1", "1");
         evaluateQuestion("answer2", "2");
@@ -100,13 +105,7 @@ $(document).ready(function () {
     });
 
 
-    function count() {
-        $(".timer").text("You have " + time + " seconds remaining.");
-        time--;
-
-        
-    }
-
+    
     function evaluateQuestion(currentQuestion, rightAnswer) {
 
         var radioValue = $("input[name='" + currentQuestion + "']:checked").val();
