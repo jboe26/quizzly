03e56645fb09bde49eec1827cdf0a2f876648754
jboe26 <joshrboepple@outlook.com>
2020-01-02 19:14:02 -0500

    hide/show questions and buttons work!

--- public/answers.js
+++ public/answers.js
@@ -2,31 +2,87 @@ $(document).ready(function () {
     console.log("ready!");
 
 
+    $("#next1").on("click", function(event) {
+        event.preventDefault();
+        $(".question1").hide();
+        $(".question2").show();
+        
+    })
+
+    $("#next2").on("click", function(event) {
+        event.preventDefault();
+        $(".question2").hide();
+        $(".question3").show();
+
+    })
+
+    $("#next3").on("click", function(event) {
+        event.preventDefault();
+        $(".question3").hide();
+        $(".question4").show();
+
+    })
+
+    $("#next4").on("click", function(event) {
+        event.preventDefault();
+        $(".question4").hide();
+        $(".question5").show();
+
+    })
+
+    $("#next5").on("click", function(event) {
+        event.preventDefault();
+        $(".question5").hide();
+        $(".question6").show();
+
+    })
+
+    $("#next6").on("click", function(event) {
+        event.preventDefault();
+        $(".question6").hide();
+        $(".question7").show();
+
+    })
+
+    $("#next7").on("click", function(event) {
+        event.preventDefault();
+        $(".question7").hide();
+        $(".question8").show();
+
+    })
+
+    $("#next8").on("click", function(event) {
+        event.preventDefault();
+        $(".question8").hide();
+        $(".question9").show();
+
+    })
+
+    $("#next9").on("click", function(event) {
+        event.preventDefault();
+        $(".question9").hide();
+        $(".question10").show();
+
+    })
+
+    $("#next10").on("click", function(event) {
+        event.preventDefault();
+        $(".question10").hide();
+        
+
+    })
+    
     var correctAnswers = 0;
     var missed = 5;
     var incorrectAnswers = 0;
     var intervalID = null;
    
 
-    //create a function to hide the start button when clicked
-    $("#startButton").click(function () {
-        //Declare variable to hold setInterval timer
-        intervalID = setInterval(count, 1000);
-        $("#startButton").hide();
 
-
-        $(".questions").hide();
-        $(".questions").show();
-
-
-
-    });
 
     $("#done").click(function () {
         $(".questions").hide();
         
-
-
         evaluateQuestion("answer1", "1");
         evaluateQuestion("answer2", "2");
         evaluateQuestion("answer3", "3");
