616a5df716940ea6a63ea586ddb724ed40848f73
Sheldon Perkins <csperkins18@gmail.com>
2020-01-04 18:43:43 -0500

    updated answers.js and all quiz pages

--- public/js/answers.js
+++ public/js/answers.js
@@ -1,129 +1,78 @@
 $(document).ready(function () {
     console.log("ready!");
 
-
-    $("#done").on("click", function (event) {
-        // Make sure to preventDefault on a submit event.
-        event.preventDefault();
-        var category = document.getElementById(idQ).value;
-        $('.slider').change(function () {
-            $('#slider_input').val(category);
-        });
-        // if (category === "english") {
-        //     $('.slider').change(function () {
-        //         $('#slider_input').val("English");
-        //     });
-        // }
-        // else if (category === "history") {
-        //     $('.slider').change(function () {
-        //         $('#slider_input').val("History");
-        //     });
-        // }
-        // else if (category === "math") {
-        //     $('.slider').change(function () {
-        //         $('#slider_input').val("Math");
-        //     });
-        // }
-        // else if (category === "science") {
-        //     $('.slider').change(function () {
-        //         $('#slider_input').val("Science");
-        //     });
-        // }
-        // else if (category === "geography") {
-        //     $('.slider').change(function () {
-        //         $('#slider_input').val("Geography");
-        //     });
-        // }
-        // else {
-        //     console.log("Not grabbing quiz id.")
-        // }
-        var grade = document.getElementById(score);
-        $('.hidden').change(function () {
-            $('#score_input').val(grade);
-        });
-
-
-    });
-
-
-    var correctAnswers = 10;
-    var missed = 10;
-    var incorrectAnswers = 10;
-    var score = 100;
-
-
     $("#next1").on("click", function (event) {
         event.preventDefault();
         $(".question1").hide();
         $(".question2").show();
-
     })
-
 
     $("#next2").on("click", function (event) {
         event.preventDefault();
         $(".question2").hide();
         $(".question3").show();
-
     })
 
     $("#next3").on("click", function (event) {
         event.preventDefault();
         $(".question3").hide();
         $(".question4").show();
-
     })
 
     $("#next4").on("click", function (event) {
         event.preventDefault();
         $(".question4").hide();
         $(".question5").show();
-
     })
 
     $("#next5").on("click", function (event) {
         event.preventDefault();
         $(".question5").hide();
         $(".question6").show();
-
     })
 
     $("#next6").on("click", function (event) {
         event.preventDefault();
         $(".question6").hide();
         $(".question7").show();
-
     })
 
     $("#next7").on("click", function (event) {
         event.preventDefault();
         $(".question7").hide();
         $(".question8").show();
-
     })
 
     $("#next8").on("click", function (event) {
         event.preventDefault();
         $(".question8").hide();
         $(".question9").show();
-
     })
 
     $("#next9").on("click", function (event) {
         event.preventDefault();
         $(".question9").hide();
         $(".question10").show();
-
     })
+    var correctAnswers = 0;
+    var missed = 10;
+    var incorrectAnswers = 0;
+    var score = 0;
 
     $("#done").on("click", function (event) {
         event.preventDefault();
-        $("#musicinfo").show();
-        $("#done").show();
-        $("#correctAnswers").show();
-        $("#missed").show();
-        $("#incorrectAnswers").show();
-        $("#home").show();
+        $(".question10").hide();
+        $(".result").show();
+        // set values for post requests
+        var category = document.getElementById(idQ).value;
+        $('.slider').change(function () {
+            $('#slider_input').val(category);
+        });
+
+        var grade = document.getElementById(score);
+        $('.hidden').change(function () {
+            $('#score_input').val(grade);
+        });
 
         evaluateQuestion("answer1", "1");
         evaluateQuestion("answer2", "2");
@@ -136,29 +85,33 @@ $(document).ready(function () {
         evaluateQuestion("answer9", "9");
         evaluateQuestion("answer10", "10");
 
-        $("#correctAnswers").text("You got " + correctAnswers + " questions right.");
-        $("#missed").text("You missed " + missed + " questions.");
-        $("#incorrectAnswers").text("You got " + incorrectAnswers + " questions wrong.");
-        $("#score").text("Your score " + score + "%")
+        cA = document.getElementById("correctAnswers");
+        catxt = document.createTextNode("You got " + correctAnswers + " questions right.");
+        cA.appendChild(catxt);
+    
+        iA = document.getElementById("incorrectAnswers");
+        iatxt = document.createTextNode("You got " + incorrectAnswers + " questions wrong.");
+        cA.appendChild(iatxt);
+    
+        mTotal = document.getElementById("missed");
+        mtxt = document.createTextNode("You missed " + missed + " questions.");
+        mTotal.appendChild(mtxt);
+    
+        scoreT = document.getElementById("score");
+        scoretxt = document.createTextNode("Your score " + score + "%");
+        scoreT.appendChild(scoretxt);
 
     });
-
-})
-
-function evaluateQuestion(currentQuestion, rightAnswer) {
-
-    var radioValue = $("input[name='" + currentQuestion + "']:checked").val();
-    if (radioValue) {
-        missed--;
-    }
-
-    if (radioValue == rightAnswer) {
-        correctAnswers++;
-        score += 10;
-    } else {
-        incorrectAnswers++;
-    }
-
-
-
-}
+    function evaluateQuestion(currentQuestion, rightAnswer) {
+        var radioValue = $("input[name='" + currentQuestion + "']:checked").val();
+        if (radioValue) {
+            missed--;
+        }
+        else if (radioValue == rightAnswer) {
+            correctAnswers++;
+            score += 10;
+        } else {
+            incorrectAnswers++;
+        }
+    };
+});
