722ac6d9bacc4d4031718ad0ddcadbf71bd641fa
Tanzania Coleman <tanzania.coleman@gmail.com>
2020-01-04 21:44:03 -0500

    removed the boilerplate app.css file. Calculator works need to add styliworks, need to add styling

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Result from './components/Results';
 import Keypad from './components/Keypad'
-import './App.css';
+
 
 
 class App extends Component {
