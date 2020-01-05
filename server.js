96568aef396b68c3e669812c79ebe1c725e01720
Sheldon Perkins <csperkins18@gmail.com>
2020-01-04 23:41:35 -0500

    passport stuff for bycrypt alerting and session

--- server.js
+++ server.js
@@ -1,18 +1,48 @@
 var express = require("express");
-// var path = require("path");
 var mongoose = require("mongoose");
-
-// const express = require('express');
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
-// const keys = require('../../config/keys_dev');
-// Sets up the Express App
-// =============================================================
+var LocalStrategy = require('passport-local');
+var session = require('express-session');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 var app = express();
+//===============EXPRESS================
+// Configure Express
+app.use(logger('combined'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(session({secret: 'supernova', saveUninitialized: true, resave: true}));
 
-var PORT = process.env.PORT || 4000;
+// Sets up the Express App
+// =============================================================
+// passport.intialize() is a middle-ware that initialise Passport. Looking at source code (https://github.com/jaredhanson/passport/blob/master/lib/middleware/initialize.js), we can see that passport.initialize() middleware basically add passport instance to incoming requests so that authentication strategy can be proceed. If there is a session, it is added to requests as well.
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Session-persisted message middleware
+app.use(function(req, res, next){
+  var err = req.session.error,
+      msg = req.session.notice,
+      success = req.session.success;
+
+  delete req.session.error;
+  delete req.session.success;
+  delete req.session.notice;
+
+  if (err) res.locals.error = err;
+  if (msg) res.locals.notice = msg;
+  if (success) res.locals.success = success;
+
+  next();
+});
+
 
 // Requiring our models for syncing
 var db = require("./models");
@@ -30,18 +60,14 @@ var MONGODB_URI = process.env.MONGODB_URI  || "mongodb://localhost/mongoHeadline
 
 mongoose.connect(MONGODB_URI);
 
-// passport.intialize() is a middle-ware that initialise Passport. Looking at source code (https://github.com/jaredhanson/passport/blob/master/lib/middleware/initialize.js), we can see that passport.initialize() middleware basically add passport instance to incoming requests so that authentication strategy can be proceed. If there is a session, it is added to requests as well.
 
-app.use(passport.initialize());
 
 //passport config
 // https://stackoverflow.com/questions/41073038/confused-about-javascript-code-require-config-passportpassport
 require('./config/passport')(passport)
 
 
-// Routes to the db
-// =============================================================
-// https://mherman.org/blog/handling-ajax-calls-with-node-dot-js-and-express-scraping-craigslist/
+//===============ROUTES=================
 // app.get('/', function(req, res) {res.render('index')});
 // app.get('/login', function(req, res) {res.render('login')});
 // app.get('/register', function(req, res) {res.render('register')});
@@ -50,9 +76,81 @@ require('./config/passport')(passport)
 // app.get('/history', function(req, res) {res.render('history')});
 // app.get('/math', function(req, res) {res.render('math')});
 // app.get('/science', function(req, res) {res.render('science')});
+
+app.post('/register', passport.authenticate('local-register', {
+  successRedirect: '/main',
+  failureRedirect: '/register'
+  })
+);
+app.post('/login', passport.authenticate('local-login', {
+  successRedirect: '/main',
+  failureRedirect: '/login'
+  })
+);
+//logs user out of site, deleting them from the session, and returns to homepage
+app.get('/logout', function(req, res){
+  var name = req.user.username;
+  console.log("LOGGIN OUT " + req.user.username)
+  req.logout();
+  res.redirect('/');
+  req.session.notice = "You have successfully been logged out " + name + "!";
+});
+
+passport.use('local-login', new LocalStrategy(
+  {passReqToCallback : true}, //allows us to pass back the request to the callback
+  function(req, username, password, done) {
+    funct.localAuth(username, password)
+    .then(function (user) {
+      if (user) {
+        console.log("LOGGED IN AS: " + user.username);
+        req.session.success = 'You are successfully logged in ' + user.username + '!';
+        done(null, user);
+      }
+      if (!user) {
+        console.log("COULD NOT LOG IN");
+        req.session.error = 'Could not log user in. Please try again.'; //inform user could not log them in
+        done(null, user);
+      }
+    })
+    .fail(function (err){
+      console.log(err.body);
+    });
+  }
+));
+passport.use('local-register', new LocalStrategy(
+  {passReqToCallback : true}, //allows us to pass back the request to the callback
+  function(req, username, password, done) {
+    funct.localReg(username, password)
+    .then(function (user) {
+      if (user) {
+        console.log("REGISTERED: " + user.username);
+        req.session.success = 'You are successfully registered and logged in ' + user.username + '!';
+        done(null, user);
+      }
+      if (!user) {
+        console.log("COULD NOT REGISTER");
+        req.session.error = 'That username is already in use, please try a different one.'; //inform user could not log them in
+        done(null, user);
+      }
+    })
+    .fail(function (err){
+      console.log(err.body);
+    });
+  }
+));
+
+// Passport session setup.
+passport.serializeUser(function(user, done) {
+  console.log("serializing " + user.username);
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  console.log("deserializing " + obj);
+  done(null, obj);
+});
+// https://mherman.org/blog/handling-ajax-calls-with-node-dot-js-and-express-scraping-craigslist/
 // register create user
-// May need to be '/users' but I think this creates the url '/login' 
-// if not then switch to user. 
 // https://expressjs.com/en/guide/routing.html
 // Load input validations
 const validateRegisterInput = require('./validation/register');
@@ -181,7 +279,8 @@ app.post('/category', (req, res) => {
     });
   });
 
-
+//===============PORT=================
+  var PORT = process.env.PORT || 4000;
     app.listen(PORT, function () {
       console.log("App listening on PORT " + PORT);
     });
