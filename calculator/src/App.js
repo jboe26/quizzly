0356e197273abc8a738c249f3f546dc5b1840506
Tanzania Coleman <tanzania.coleman@gmail.com>
2020-01-04 20:48:17 -0500

    imported keypad and result into app. set the state of result on calculator to empty string

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Result from './components/Results';
+import Keypad from './components/Keypad'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor(){
+  super();
+
+  this.state = {
+    result: ""
+  }
+  }
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default App;
