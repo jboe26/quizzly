552d1bc3a7a72f1d5b5e3d8a50174e371abc8912
Sheldon Perkins <csperkins18@gmail.com>
2020-01-08 01:15:13 -0500

    answers.js fix english.html fix

--- public/js/answers.js
+++ public/js/answers.js
@@ -1,137 +1,125 @@
 $(document).ready(function () {
-    console.log("ready!");
-
-    var correctAnswers = 0;
-    var missed = 0;
-    var incorrectAnswers = 0;
-    var score = 0;
-
+    // Start button to show first question
+    $("#start").on("click", function (event) {
+        $("#start").hide();
+        $("#q1").show();
+    });
+    // A series of questions that shows based on previous button; hides on #next[i] button
     $("#next1").on("click", function (event) {
-        event.preventDefault();
-        $(".question1").hide();
-        $(".question2").show();
+        $("#q1").hide();
+        $("#q2").show();
     });
-
     $("#next2").on("click", function (event) {
-        event.preventDefault();
-        $(".question2").hide();
-        $(".question3").show();
-
+        $("#q2").hide();
+        $("#q3").show();
     });
-
     $("#next3").on("click", function (event) {
-        event.preventDefault();
-        $(".question3").hide();
-        $(".question4").show();
+        $("#q3").hide();
+        $("#q4").show();
     });
-
     $("#next4").on("click", function (event) {
-        event.preventDefault();
-        $(".question4").hide();
-        $(".question5").show();
+        $("#q4").hide();
+        $("#q5").show();
     });
-
     $("#next5").on("click", function (event) {
-        event.preventDefault();
-        $(".question5").hide();
-        $(".question6").show();
+        $("#q5").hide();
+        $("#q6").show();
     });
-
     $("#next6").on("click", function (event) {
-        event.preventDefault();
-        $(".question6").hide();
-        $(".question7").show();
+        $("#q6").hide();
+        $("#q7").show();
     });
-
     $("#next7").on("click", function (event) {
-        event.preventDefault();
-        $(".question7").hide();
-        $(".question8").show();
+        $("#q7").hide();
+        $("#q8").show();
     });
-
     $("#next8").on("click", function (event) {
-        event.preventDefault();
-        $(".question8").hide();
-        $(".question9").show();
+        $("#q8").hide();
+        $("#q9").show();
     });
-
     $("#next9").on("click", function (event) {
-        event.preventDefault();
-        $(".question9").hide();
-        $(".question10").show();
+        $("#q9").hide();
+        $("#q10").show();
     });
+    // Answer Key
+    var answers = {
+        "answer1": "1",
+        "answer2": "1",
+        "answer3": "1",
+        "answer4": "1",
+        "answer5": "1",
+        "answer6": "1",
+        "answer7": "1",
+        "answer8": "1",
+        "answer9": "1",
+        "answer10": "1"
+    };
 
     $("#done").on("click", function (event) {
-        
-        $(".question10").hide();
-        $("#result").show();
-        // set values for post requests
-       
-        var category = document.getElementById(idQ);
-        $('.slider').change(function () {
-            $('#slider_input').val(category);
+        // Results variables to be displayed in results div based on user performance
+        var correctAnswers = 0;
+        var missed = 0;
+        var incorrectAnswers = 0;
+        var score = 0;
+        // Hides question10
+        $("#q10").hide();
+        // $("#result").show();
+        // Functions to increment results variables
+        function markIncorrect() {
+            missed++;
+            incorrectAnswers++;
+            console.log("Hit INCO")
+        };
+        function markCorrect() {
+            correctAnswers++;
+            score += 10;
+            console.log("HITCO")
+        };
+
+        // on #done click get user input check if Answered. Check if answer is not correct. Else answer is correct.
+        $questions = $(".question");
+        $questions.each(function () {
+            var answer = $(this).find("input:checked"),
+                key = answer.attr("name"),
+                val = answer.attr("id");
+
+            if (answer.length === 0) {
+                markIncorrect();
+                console.log("inco");
+                console.log(missed);
+            } else if (answers[key] !== val) {
+                markIncorrect();
+                console.log("i$$$$nco");
+            } else {
+                markCorrect();
+                console.log("CO");
+            }
         });
+        // For post route to the database for quiz and result
+        // var category = document.getElementById('idQ');
+        // $('.slider').change(function () {
+        //     $('#slider_input').val(category);
+        // });
 
-        var grade = document.getElementById(score);
-        $('.hidden').change(function () {
-            $('#score_input').val(grade);
-        });
+        // var grade = document.getElementById('score');
+        // $('.hidden').change(function () {
+        //     $('#score_input').val(grade);
+        // });
 
-        evaluateQuestion("answer1", "1");
-        evaluateQuestion("answer2", "2");
-        evaluateQuestion("answer3", "3");
-        evaluateQuestion("answer4", "4");
-        evaluateQuestion("answer5", "5");
-        evaluateQuestion("answer6", "6");
-        evaluateQuestion("answer7", "7");
-        evaluateQuestion("answer8", "8");
-        evaluateQuestion("answer9", "9");
-        evaluateQuestion("answer10", "10");
-
+        //    Give html elements text from variables linked to quiz performance
         cA = document.getElementById("correctAnswers");
         catxt = document.createTextNode("You got " + correctAnswers + " questions right.");
         cA.appendChild(catxt);
-
         iA = document.getElementById("incorrectAnswers");
         iatxt = document.createTextNode("You got " + incorrectAnswers + " questions wrong.");
         cA.appendChild(iatxt);
-
         mTotal = document.getElementById("missed");
         mtxt = document.createTextNode("You missed " + missed + " questions.");
         mTotal.appendChild(mtxt);
-
         scoreT = document.getElementById("score");
         scoretxt = document.createTextNode("Your score " + score + "%");
         scoreT.appendChild(scoretxt);
-
+        // Shows results div
+        $("#result").show();
     });
-    // These variables haven't been declared
-    // We need to tell them what they are getting from the user and the correct answer
-    // // var rightAnswer = document.getElementById(this.correct) or something like this
-    // This has to be ran on every question unless answers bc we are not doing form submit.
-    // https://stackoverflow.com/questions/9618504/how-to-get-the-selected-radio-button-s-value
-     var rightAnswer = document.getElementsByName().value
-    var userAnswer = document.getElementsByName('answers10');
-    for (var i = 0; i < userAnswer.length; i++) {
-        if (userAnswer[i].checked && rightAnswer) {
-          // do whatever you want with the checked radio
-          alert(radios[i].value);
-      
-          // only one radio can be logically checked, don't check the rest
-          break;
-        }
-      }
-    // It's late and I'm too tired to fool with this. 
-    // We need to set a loop to get the current question and check if the users "checked" and value are the same 
-    // this function has to be ran on each question
-    function evaluateQuestion(currentQuestion, rightAnswer) {
-        var radioValue = $("input[name='" + currentQuestion + "']:checked").val();
-        if (radioValue != rightAnswer) {
-            missed++;
-            incorrectAnswers++;
-        } else {
-            correctAnswers++;
-            score += 10;
-        };
-    };
 });
